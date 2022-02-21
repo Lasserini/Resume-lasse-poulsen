c37558b6818e3b80d002db2cc5beefc2bb45c8bd
Lasserini <lasserini@gmail.com>
2022-02-21 22:58:09 +0000

    Fix goggle maps clustering functionality

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,29 +1,24 @@
 function initMap() {
-    var map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 3,
-        center: {
-            lat: 55.99436810278571,
-            lng: 11.969633027784207
-        }
+    const map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 4,
+        center: { lat: 55.99436810278571, lng: 11.969633027784207 }
     });
-
-    var labels = "ABCDEFGHIJKLMOPQRSTUVWXYZ";
-
-    var locations = [
-        { lat: 55.99709525043145, lgn: 11.970390676145506 },
-        { lat: 55.68117682345807, lgn: 12.577199142139685 },
-        { lat: 55.66300284834, lgn: 12.589074539941048 },
-        { lat: 55.68222589393442, lgn: 12.576619894236746 },
-        { lat: 53.29834615774438, lgn: -6.178096102150207 },
-        { lat: -17.821700445971388, lgn: 31.04967577367341 }
+    
+    const locations = [
+        { lat: 55.99709525043145, lng: 11.970390676145506 },
+        { lat: 55.68117682345807, lng: 12.577199142139685 },
+        { lat: 55.66300284834, lng: 12.589074539941048 },
+        { lat: 55.68222589393442, lng: 12.576619894236746 },
+        { lat: 53.29834615774438, lng: -6.178096102150207 },
+        { lat: -17.821700445971388, lng: 31.04967577367341 },
     ];
-
-    var markers = locations.map(function(location, i) {
-        return new google.maps.Marker({
+  
+    const markers = locations.map(function (location, i) {
+      const labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+      return new google.maps.Marker({
         position: location,
-        label: labels[i % labels.length]
-        });
+        label: labels[i % labels.length],
+      });
     });
-
-    var markerCluster = new MarkerClusterer(map, markers, { imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m' });
-}
+    const markerCluster = new markerClusterer.MarkerClusterer({map, markers});
+  }
--- assets/js/maptest.js
+++ assets/js/maptest.js
@@ -40,3 +40,31 @@ const locations = [
     { lat: 53.29834615774438, lgn: -6.178096102150207 },
     { lat: -17.821700445971388, lgn: 31.04967577367341 }
 ];
+
+var markerCluster = new MarkerClusterer(map, markers, { imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m' });
+
+
+function initMap() {
+  const map = new google.maps.Map(document.getElementById("map"), {
+      zoom: 4,
+      center: { lat: 55.99436810278571, lng: 11.969633027784207 }
+  });
+
+  const locations = [
+      { lat: 55.99709525043145, lng: 11.970390676145506 },
+      { lat: 55.68117682345807, lng: 12.577199142139685 },
+      { lat: 55.66300284834, lng: 12.589074539941048 },
+      { lat: 55.68222589393442, lng: 12.576619894236746 },
+      { lat: 53.29834615774438, lng: -6.178096102150207 },
+      { lat: -17.821700445971388, lng: 31.04967577367341 }
+  ];
+
+  const markers = locations.map(function (location, i) {
+      const labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+      return new google.maps.Marker({
+          position: location,
+          label: labels[i % labels.length],
+      });
+    });
+  const markerCluster = new markerClusterer.MarkerClusterer({map, markers});
+}
