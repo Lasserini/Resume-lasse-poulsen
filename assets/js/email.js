3407247c876855f325af19003e19dd345bbd88b8
Lasserini <lasserini@gmail.com>
2022-02-22 13:33:42 +0000

    Add user response to contact page formular submit button

--- assets/js/email.js
+++ assets/js/email.js
@@ -7,9 +7,17 @@ function sendMail(contactForm) {
     .then(
         function(response) {
             console.log("SUCCESS", response);
+            document.getElementById("submit_button").style.color = "#fff";
+            document.getElementById("submit_button").style.backgroundColor = "#14921a";
+            document.getElementById("submit_button").innerHTML = "SUCCESS!";
+            document.getElementById("submit_button").fontWeight = "700";
         },
         function(error) {
             console.log("FAILED", error);
+            document.getElementById("submit_button").style.color = "#fff";
+            document.getElementById("submit_button").style.backgroundColor = "red";
+            document.getElementById("submit_button").innerHTML = "Request Failed, send regular email instead";
+            document.getElementById("submit_button").fontWeight = "700";
         }
     );
     return false;  // To prevent loading a new page
